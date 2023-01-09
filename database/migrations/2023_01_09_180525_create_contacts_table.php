3d9a522e1a9b5e0ef3b5f544e4e151fc460a3815
Musanzi <musanziw.m@gmail.com>
2023-01-09 20:32:35 +0200

    [ADD] Add models and migrations for contact, group and message

---
+++ database/migrations/2023_01_09_180525_create_contacts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('address');
+            $table->boolean('status')->default(true);
+            $table->foreignId('group_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
---
+++ database/migrations/2023_01_09_180731_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->string('document')->nullable();
+            $table->foreignId('user_id');
+            $table->foreignId('contact_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};

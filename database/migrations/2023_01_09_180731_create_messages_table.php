b3972d5a81f583172039b837272228ef208456d2
Musanzi <musanziw.m@gmail.com>
2023-01-10 18:29:18 +0200

    [DELETE] Remove document attribute,

--- database/migrations/2023_01_09_180731_create_messages_table.php
+++ database/migrations/2023_01_09_180731_create_messages_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('content');
-            $table->string('document')->nullable();
             $table->foreignId('user_id');
             $table->foreignId('contact_id');
             $table->timestamps();

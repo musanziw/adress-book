53ea42008f64d22609ce2823e6516f099f84fdb0
Musanzi <musanziw.m@gmail.com>
2023-01-11 17:19:07 +0200

    [UPDATE] Update the path to the app.js file.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,14 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    @vite(['public/css/dashlite.min.css', 'public/js/bundle.js', 'public/js/scripts.js', 'resources/css/app.js',])
+    @vite(['public/css/dashlite.min.css', 'public/js/bundle.js', 'public/js/scripts.js', 'resources/js/app.js',])
 </head>
 <body class="nk-body bg-white has-sidebar">
 <div class="nk-app-root">
     <div class="nk-main">
         @include('partials.sidebar')
         <div class="nk-wrap ">
-            {{ $slot }}
+            @include('partials.header')
+            <div class="nk-content nk-content-fluid mr-10">
+                <div class="container-xl wide-lg">
+                    <div class="nk-content-body">
+                        {{ $slot }}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>

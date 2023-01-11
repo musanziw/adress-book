d8f01505d9019ef74e4d215620cc1cb7196d7aa8
Musanzi <musanziw.m@gmail.com>
2023-01-11 17:18:02 +0200

    [REFACTOR] Remove unused attribute.

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -3,11 +3,9 @@
 <div class="example-alert mt-2 mb-2">
     <div class="alert alert-danger alert-icon">
         @if ($messages)
-            <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
-                @foreach ((array) $messages as $message)
-                    {{ $message }}
-                @endforeach
-            </ul>
+            @foreach ((array) $messages as $message)
+                {{ $message }}
+            @endforeach
         @endif
     </div>
 </div>

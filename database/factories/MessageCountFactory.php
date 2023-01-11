26ef8d888299450cbbd3bfcb55b74f9737d8c7bd
Musanzi <musanziw.m@gmail.com>
2023-01-11 17:16:02 +0200

    [CREATE] Add Group factory et MessageCount factory.

---
+++ database/factories/MessageCountFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\MessageCount;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<MessageCount>
+ */
+class MessageCountFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'sent_emails' => 10,
+            'available_emails' => 20,
+            'sent_sms' => 15,
+            'available_sms' => 15
+        ];
+    }
+}

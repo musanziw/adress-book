7cefb95bba2bc022f22201a5e6a438309e78dfa6
Musanzi <musanziw.m@gmail.com>
2023-01-11 17:21:52 +0200

    [UPDATE] Add the first and last names properties.

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'firstname',
+        'lastname',
         'name',
         'email',
         'password',

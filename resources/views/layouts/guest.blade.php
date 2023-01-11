5ef137d772a77d63d331c04913d21d94e5e54d80
Musanzi <musanziw.m@gmail.com>
2023-01-11 17:24:07 +0200

    [UPDATE] Update the path to the app.js file.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Maadini') }}</title>
-    @vite(['public/css/dashlite.min.css', 'public/js/bundle.js', 'public/js/scripts.js', 'resources/css/app.js',])
+    @vite(['public/css/dashlite.min.css', 'public/js/bundle.js', 'public/js/scripts.js', 'resources/js/app.js',])
 </head>
 <body class="nk-body bg-white npc-general pg-auth">
 <div class="nk-app-root">
     <div class="nk-main ">
         <div class="nk-wrap nk-wrap-nosidebar">
             <div class="nk-content">
-                <div class="nk-block nk-block-middle nk-auth-body  wide-xs">
+                <div class="nk-block nk-block-middle nk-auth-body wide-xs">
                     {{ $slot }}
                 </div>
             </div>

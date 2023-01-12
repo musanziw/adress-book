7a9508b2e1ad8ff06cd9533dcc9df993c7933ca4
Musanzi <musanziw.m@gmail.com>
2023-01-12 11:54:53 +0200

    [ADD] CRUD for groups.

--- resources/views/group/index.blade.php
+++ resources/views/group/index.blade.php
@@ -1,11 +1,37 @@
 <x-app-layout>
 
+    <div class="nk-block-head nk-block-head-sm">
+        <div class="nk-block-between">
+            <div class="nk-block-head-content">
+                <h4 class="nk-block-title">Liste des groups disponibles</h4>
 
-    <div class="nk-block-head">
-        <div class="nk-block-head-content">
-            <h4 class="nk-block-title">Liste des groups disponibles</h4>
+            </div>
+
+            <div class="nk-block-head-content">
+                <a href="{{ route('group.create') }}" class="btn btn-white btn-dim btn-outline-primary">
+                    <em class="icon ni ni-plus"></em><span>Add group</span>
+                </a>
+            </div>
         </div>
+
+        @if(session('status') === 'group-upated')
+            <x-input-success>
+                {{ __('Group updated successfully') }}
+            </x-input-success>
+        @endif
+        @if(session('status') === 'group-created')
+            <x-input-success>
+                {{ __('Group created successfully') }}
+            </x-input-success>
+        @endif
+        @if(session('status') === 'group-deleted')
+            <x-input-success>
+                {{ __('Group deleted successfully') }}
+            </x-input-success>
+        @endif
+
     </div>
+
 
     <div class="card card-bordered card-preview">
         <table class="table">
@@ -13,20 +39,14 @@
             <tr class="tb-tnx-head">
                 <th class="tb-tnx-id"><span class="">#</span></th>
                 <th class="tb-tnx-info">
-                        <span class="tb-tnx-desc d-none d-sm-inline-block">
-                            <span>Nom</span>
-                        </span>
+                    <span class="tb-tnx-desc d-none d-sm-inline-block">Nom</span>
                 </th>
-
                 <th class="tb-tnx-info">
-                        <span class="tb-tnx-desc d-none d-sm-inline-block">
-                            Actions
-                        </span>
+                    <span class="tb-tnx-desc d-none d-sm-inline-block">Actions</span>
                 </th>
             </tr>
             </thead>
             <tbody>
-
 
             @foreach($groups as $group)
                 <tr class="tb-tnx-item">
@@ -41,15 +61,17 @@
 
                     <td class="tb-tnx-info">
                         <div class="tb-tnx-desc">
-                            <a href="" class="btn btn-primary">Edition</a>
-                            <form action="" method="post" style="display: inline-block">
+                            <a href="{{ route('group.edit', ['id' => $group->id]) }}"
+                               class="btn btn-primary">Edition</a>
+                            <form action="{{ route('group.delete', ['id' => $group->id]) }}" method="post" style="display: inline-block" onclick="return confirm('Are sur ?')">
+                                @csrf
+                                @method('DELETE')
                                 <button type="submit" class="btn btn-danger">Supprimer</button>
                             </form>
                         </div>
                     </td>
                 </tr>
             @endforeach
-
 
             </tbody>
         </table>

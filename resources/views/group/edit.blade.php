7b481e8509ab4cbde23d3440d0565994aa16fe07
Musanzi <musanziw.m@gmail.com>
2023-01-12 11:54:10 +0200

    [ADD] Create add edit view.

---
+++ resources/views/group/edit.blade.php
@@ -0,0 +1,46 @@
+<x-app-layout>
+    <div class="card card-bordered card-preview">
+        <div class="card-inner">
+            <div class="preview-block">
+                <span class="preview-title-sm overline-title">{{ $group->name }}</span>
+
+                @if($errors->any())
+                    @foreach($errors->all() as $messages)
+                        <x-input-error :messages="$messages"/>
+                    @endforeach
+                @endif
+
+                <form action="" method="post">
+                    @csrf
+                    <div class="row gy-4 mt-3">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label class="form-label" for="description">Description</label>
+                                <div class="form-control-wrap">
+                                <textarea class="form-control no-resize" id="description"
+                                          name="description">{{ old('description', $group->description) }}</textarea>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 mb-4">
+                            <div class="form-group">
+                                <x-input-label for="name" :value="__('Group Name')"/>
+                                <div class="form-control-wrap">
+                                    <x-text-input name="name" value="{{ old('name', $group->name) }}"/>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <x-primary-button>
+                                    {{ __('Ediion du groupe') }}
+                                </x-primary-button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>

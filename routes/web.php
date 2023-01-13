f5db0adff5776139ed69c04067bbb8d5cf9f6ec7
Musanzi <musanziw.m@gmail.com>
2023-01-13 19:39:50 +0200

    [ADD] Add contact CRUD routes.

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Group routes
     Route::get('/groups', [GroupController::class, 'index'])->name('groups.index');
     Route::get('/group/edit/{id}', [GroupController::class, 'edit'])->name('group.edit');
     Route::post('/group/edit/{id}', [GroupController::class, 'update']);
     Route::get('/group/create', [GroupController::class, 'create'])->name('group.create');
     Route::post('/group/create', [GroupController::class, 'store']);
     Route::delete('/group/deleted/{id}', [GroupController::class, 'delete'])->name('group.delete');
+
+    // Contact routes
     Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/contact/edit/{id}', [ContactController::class, 'edit'])->name('contact.edit');
+    Route::post('/contact/edit/{id}', [ContactController::class, 'update']);
+    Route::get('/contact/create', [ContactController::class, 'create'])->name('contact.create');
+    Route::post('/contact/create', [ContactController::class, 'store']);
+    Route::delete('/contact/delete/{id}', [ContactController::class, 'delete'])->name('contact.delete');
+
+    // Message routes
     Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
 });
 

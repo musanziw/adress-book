015437495a9cbd6daa4a2f68f595020ab710d56d
Musanzi <musanziw.m@gmail.com>
2023-01-13 11:22:12 +0200

    [ADD] add a relationship with contacts.

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Group extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function contacts(): BelongsToMany
+    {
+        return $this->belongsToMany(ContactGroup::class, 'contact_group');
+    }
 }
--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,28 +1,41 @@
 <x-app-layout>
 
+    <div class="nk-block-head nk-block-head-sm">
+        <div class="nk-block-between">
+            <div class="nk-block-head-content">
+                <h4 class="nk-block-title">Liste des groups disponibles</h4>
 
-    <div class="nk-block-head">
-        <div class="nk-block-head-content">
-            <h4 class="nk-block-title">Liste des groups disponibles</h4>
+            </div>
+
+            <div class="nk-block-head-content">
+                <a href="{{ route('contact.create') }}" class="btn btn-white btn-dim btn-outline-primary">
+                    <em class="icon ni ni-plus"></em><span>Add contact</span>
+                </a>
+            </div>
         </div>
+
+        @if(session('status') === 'contact-created')
+            <x-input-success :message="__('contact created successfully')"/>
+        @endif
+        @if(session('status') === 'group-created')
+            <x-input-success :message="__('Group created successfully') "/>
+        @endif
+        @if(session('status') === 'group-deleted')
+            <x-input-success :message=" __('Group deleted successfully')"/>
+        @endif
+
     </div>
 
     <div class="card card-bordered card-preview">
         <table class="table">
             <thead>
             <tr class="tb-tnx-head">
-                <th class="tb-tnx-id"><span class="">#</span></th>
-                <th class="tb-tnx-info">
-                        <span class="tb-tnx-desc d-none d-sm-inline-block">
-                            <span>Nom</span>
-                        </span>
-                </th>
-
-                <th class="tb-tnx-info">
-                        <span class="tb-tnx-desc d-none d-sm-inline-block">
-                            Actions
-                        </span>
-                </th>
+                <th class="tb-tnx-id">#</th>
+                <th class="tb-tnx-info">Firstname</th>
+                <th class="tb-tnx-info">name</th>
+                <th class="tb-tnx-info">Lastname</th>
+                <th class="tb-tnx-info">Groups</th>
+                <th class="tb-tnx-info">Actions</th>
             </tr>
             </thead>
             <tbody>
@@ -30,22 +43,26 @@
 
             @foreach($contacts as $contact)
                 <tr class="tb-tnx-item">
-                    <td class="tb-tnx-id">
-                        <a href="#">{{ $contact->id }}</a>
-                    </td>
+                    <td class="tb-tnx-id">{{ $contact->id }}</td>
+                    <td class="tb-tnx-info">{{ $contact->firstname }}</td>
+                    <td class="tb-tnx-info">{{ $contact->name }}</td>
+                    <td class="tb-tnx-info">{{ $contact->lastname }}</td>
                     <td class="tb-tnx-info">
-                        <div class="tb-tnx-desc">
-                            <span class="title">{{ $contact->name }}</span>
-                        </div>
+                        @foreach($contact->groups as $group)
+                           <span style="display: block">
+                               {{ $group->name }}
+                           </span>
+                        @endforeach
                     </td>
 
+
+
                     <td class="tb-tnx-info">
-                        <div class="tb-tnx-desc">
                             <a href="" class="btn btn-primary">Edition</a>
                             <form action="" method="post" style="display: inline-block">
                                 <button type="submit" class="btn btn-danger">Supprimer</button>
                             </form>
-                        </div>
+
                     </td>
                 </tr>
             @endforeach

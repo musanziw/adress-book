64d5d5499e4d53e57c8a4910f305769ece691ebf
Musanzi <musanziw.m@gmail.com>
2023-01-14 17:57:14 +0200

    [ADD] Add contacts, groups and messageCount relationShip.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -43,4 +43,26 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the contacts for the user.
+     */
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class);
+    }
+
+    /**
+     * Get the groups for the user.
+     */
+    public function groups(): HasMany
+    {
+        return $this->hasMany(Group::class);
+    }
+
+    public function messageCount(): HasMany
+    {
+        return $this->hasMany(MessageCount::class);
+    }
 }

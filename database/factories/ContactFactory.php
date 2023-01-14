fe68da172f86465b3efeb25aa87c5a6fd965e81d
Musanzi <musanziw.m@gmail.com>
2023-01-14 17:59:42 +0200

    [CREATE] Create ContactFactory.

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'firstname' => $this->faker->firstName,
+            'name' => $this->faker->name,
+            'lastname' => $this->faker->lastName,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'status' => $this->faker->boolean,
+            'user_id' => $this->faker->numberBetween(1, 2),
+        ];
+    }
+}

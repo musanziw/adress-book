d2bc76d8f856d270a2c2167e3d61e8a5da1fcb3f
Musanzi <musanziw.m@gmail.com>
2023-01-16 21:14:43 +0200

    [UPDATE] Update card style.

--- resources/views/components/dashboard/card.blade.php
+++ resources/views/components/dashboard/card.blade.php
@@ -1,10 +1,17 @@
 @props(['message', 'description'])
 
-<div class="nk-order-ovwg-data sell">
-    <div class="amount">
-        {{ $message }}
-    </div>
-    <div class="info">
-        {{ $description }}
-    </div>
+<div class="card card-bordered">
+    <div class="card-inner">
+        <div class="team">
+
+            <div class="user-card user-card-s2">
+                <div class="user-avatar md bg-primary">
+                    <span>{{ $message }}</span>
+                </div>
+                <div class="user-info">
+                    <h6>{{ $description }}</h6>
+                </div>
+            </div>
+        </div><!-- .team -->
+    </div><!-- .card-inner -->
 </div>

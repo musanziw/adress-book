b689a979dc44d3d25a33308a03f46c1d2da522ae
Musanzi <musanziw.m@gmail.com>
2023-01-16 21:19:49 +0200

    [DELETE] Delete contact id attribute.

--- database/migrations/2023_01_09_180731_create_messages_table.php
+++ database/migrations/2023_01_09_180731_create_messages_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('title');
             $table->string('content');
             $table->foreignId('user_id');
-            $table->foreignId('contact_id');
             $table->timestamps();
         });
     }

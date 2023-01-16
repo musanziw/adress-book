9349699d9267de9d327e3dee29d60e3993d23cb9
Musanzi <musanziw.m@gmail.com>
2023-01-16 14:24:08 +0200

    [ADD] Add the message belongs to relationship.

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -13,13 +13,27 @@ class Group extends Model
 
     protected $guarded = [];
 
+    /**
+     * Get the contacts that belongs to a group
+     */
     public function contacts(): BelongsToMany
     {
         return $this->belongsToMany(Contact::class, ContactGroup::class)->withTimestamps();
     }
 
+    /**
+     * Get the user that owns the Group
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the messages for the group.
+     */
+    public function messages(): BelongsToMany
+    {
+        return $this->belongsToMany(Message::class, GroupMessage::class)->withTimestamps();
     }
 }
